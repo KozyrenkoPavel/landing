9911e145f9d9c2b13ccef36d51c434371fd468fd
Pavel Kozyrenko <kozyrenko_pavlik@mail.ru>
2023-11-04 14:51:14 +0300

    Error correction

--- index.js
+++ index.js
@@ -2,30 +2,40 @@ const buttonMenu = document.querySelector('.navigation__button--menu');
 const menuItems = document.querySelector('.navigation__adaptive');
 const menuAllItem = document.querySelectorAll('.navigation__button--adaptive');
 const menuNavigation = document.querySelector('.navigation');
+const applicationButton = document.querySelector('.navigation__adaptive--application')
+
+menuItems.style.display = 'none';
 
 buttonMenu.addEventListener('click', (event) => {
-  buttonMenu.style.display = 'none';
-  menuItems.style.display = 'block';
+  if (menuItems.style.display === 'none') {
+    menuItems.style.display = 'block';
+  } else {
+    menuItems.style.display = 'none';
+  }
+
   menuNavigation.style.display = 'none';
 });
 
 menuAllItem.forEach((item) => {
   item.addEventListener('click', (event) => {
-    buttonMenu.style.display = 'flex';
     menuItems.style.display = 'none';
     menuNavigation.style.display = 'none';
   });
 });
 
+applicationButton.addEventListener('click', event => {
+    menuItems.style.display = 'none';
+    menuNavigation.style.display = 'none';    
+})
+
 const chandeWidth = function (width) {
   let newWigth = Number(width.replace('px', ''));
 
   if (newWigth < 1000) {
-    buttonMenu.style.display = 'flex';
     menuNavigation.style.display = 'none';
   } else {
-    buttonMenu.style.display = 'none';
     menuNavigation.style.display = 'flex';
+    menuItems.style.display = 'none'
   }
 };
 
